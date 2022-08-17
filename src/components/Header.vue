d2b6a1c5b7682b37b4ac5a629202af7dce33bf57
carlo <jpayabyab@teko.ph>
2022-08-17 22:21:47 +0800

    init

---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-container class="mx-16" fluid>
+    <v-row justify="center">
+      <v-col cols="4" class="top">
+        <h1>Your first step to achieving your dream home</h1>
+        <p>
+          Get to know what you can afford. Try calculating your home loan
+          monthly amortization
+        </p>
+        <v-btn rounded small color="#CCEF85"> Try Ho-Lo for free </v-btn>
+      </v-col>
+      <v-col cols="4">
+        <v-img max-height="559" max-width="495" src="@/assets/header.png" />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.top {
+  margin-top: 5%;
+}
+
+</style>
